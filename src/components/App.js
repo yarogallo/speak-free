bd3e33db2d5ef05a3c3d6288615c76ea3a500cf2
yanisleidi <yaroga88@yahoo.com>
2018-07-30 17:17:32 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="container">
+        
+      </section>
+    );
+  }
+}
+
+export default App;
