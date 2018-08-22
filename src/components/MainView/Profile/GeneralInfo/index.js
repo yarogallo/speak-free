ae4062aaa12782c6943920e56138f33cb05a2c78
yanisleidi <yaroga88@yahoo.com>
2018-08-21 22:36:57 -0700

    Modified GeneralInfo component

--- src/components/MainView/Profile/GeneralInfo/index.js
+++ src/components/MainView/Profile/GeneralInfo/index.js
@@ -3,44 +3,65 @@ import PropTypes from 'prop-types';
 import './style/style.css';
 
 function GeneralInfo(props) {
+	const TABS = {
+		options: 'options',
+		following: 'following',
+		followers: 'followers',
+		likes: 'likes'
+	};
+	
 	const { 
 		numbOpinions,
 		followersNumb,
 		followingNumb,
 		likesNumb
 	} = props.generalInfo;
+	
+	const {
+		changeTabHandler,
+		selectedTab
+	} = props;
+	
+	const setClass = (tab) => {
+		return tab === selectedTab ? 'elem selected': 'elem';
+	};
+	
 	return(
 		<ul className="general-list">
-			<li className="elem">
-				<div className="content">
+			<li className={setClass(TABS.options)} >
+				<button 
+					className="content"
+					onClick={ () => { changeTabHandler(TABS.options)} }>
 					<span className="text">opinions</span>
 					<span className="number">{numbOpinions}</span>
-				</div>
+				</button>
 			</li>
 			<li className="elem">
-				<div className="content">
+				<button className="content" onClick={() => changeTabHandler(TABS.following)}>
 					<span className="text">following</span>
 					<span className="number">{followingNumb}</span>
-				</div>
+				</button>
 			</li>
 			<li className="elem">
-				<div className="content">
+				<button className="content" onClick={() => changeTabHandler(TABS.followers)}>
 					<span className="text">followers</span>
 					<span className="number">{followersNumb}</span>
-				</div>
+				</button>
 			</li>
-			<li className="elem">
-				<div className="content">
+			<li className="elem" onClick={() => {changeTabHandler(TABS.likes)}}>
+				<button className="content">
 					<span className="text">likes</span>
 					<span className="number">{likesNumb}</span>
-				</div>
+				</button>
 			</li>
 		</ul>
 	);
 }
 
 GeneralInfo.propTypes = {
-	generalInfo: PropTypes.object.isRequired
+	generalInfo: PropTypes.object.isRequired,
+	changeTabHandler: PropTypes.func,
+	selectedTab: PropTypes.string
 }
 
 GeneralInfo.defaultProps = {
@@ -49,7 +70,9 @@ GeneralInfo.defaultProps = {
 		followingNumb: 0,
 		followersNumb: 0,
 		likesNumb: 0
-	}
+	},
+	changeTabHandler: () => {},
+	selectedTab: 'options'
 }
 
 export default GeneralInfo;
