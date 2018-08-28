d50fb04c7f202359c3e62d0c8556d76ae3dbca6f
yanisleidi <yaroga88@yahoo.com>
2018-08-28 14:34:55 -0700

    Changed main header style

--- src/components/main-view/header-view/index.js
+++ src/components/main-view/header-view/index.js
@@ -1,37 +1,24 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Logo from './images/speech-bubbles.svg';
 import './header-view.css';
 import SearchForm from '../../search-form';
 
 class HeaderView extends Component {
 	render() {
-		const {onSearchOpinions} = this.props;
 		return(
-			<header className="header-view-container">
-				<div className="header-view">
-								
-					<nav className="header-view-navigation">
-						<a href="#" className="header-view-navigation__item"><span>home</span></a>
-						<a href="#" className="header-view-navigation__item"><span>say something</span></a>
-					</nav>
-					
-					<div className="header-view-logo header-view-logo_center">
-						<img src={Logo} alt="main logo"/>
-					</div>
-					
-					<div  className="header-view-search header-view-search_center">	
-						<SearchForm text="search opinions" onPressSearch={onSearchOpinions}/>
-					</div>
-					
-				</div>
+			<header className="header-view">
+				<nav className="header-view-navigation">
+					<a href="#" className="header-view-navigation__item header-view-navigation__item_blue">home</a>
+					<a href="#" className="header-view-navigation__item header-view-navigation__item_lila">profile</a>
+					<a href="#" className="header-view-navigation__item header-view-navigation__item_red">comment</a>
+				</nav>
 			</header>
 		);
 	}
 }
 
 HeaderView.propTypes = {
-	onSearchOpinions: PropTypes.func
+	
 }
 
 export default HeaderView;
