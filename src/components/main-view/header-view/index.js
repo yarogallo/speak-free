5288cf53c5a0e986b5b53348b9c8d1cf626a74ae
yanisleidi <yaroga88@yahoo.com>
2018-08-28 14:49:45 -0700

    defined an active tab in main header

--- src/components/main-view/header-view/index.js
+++ src/components/main-view/header-view/index.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './header-view.css';
-import SearchForm from '../../search-form';
 
 class HeaderView extends Component {
 	render() {
+		const TABS = {
+			home: 'home',
+			profile: 'profile',
+			comment: 'comment'
+		};
+		const {
+			activeTab
+		} = this.props;
+		
+		const borderBottom = {
+			borderColor: "var(--main-color-gray)" 
+		};
 		return(
 			<header className="header-view">
 				<nav className="header-view-navigation">
-					<a href="#" className="header-view-navigation__item header-view-navigation__item_blue">home</a>
-					<a href="#" className="header-view-navigation__item header-view-navigation__item_lila">profile</a>
-					<a href="#" className="header-view-navigation__item header-view-navigation__item_red">comment</a>
+					<a href="#" 
+						className="header-view-navigation__item header-view-navigation__item_blue"
+						style={activeTab === TABS.home ? borderBottom : {}}>home</a>
+					<a 
+						href="#" 
+						className="header-view-navigation__item header-view-navigation__item_lila"
+						style={activeTab === TABS.profile ? borderBottom : {}}>profile</a>
+					<a 
+						href="#" 
+						className="header-view-navigation__item header-view-navigation__item_red"
+						style={activeTab === TABS.comment ? borderBottom : {}}>comment</a>
 				</nav>
 			</header>
 		);
@@ -18,7 +37,11 @@ class HeaderView extends Component {
 }
 
 HeaderView.propTypes = {
-	
+	activeTab: PropTypes.string.isRequired,
+}
+
+HeaderView.defaultProps = {
+	activeTab: 'home'
 }
 
 export default HeaderView;
